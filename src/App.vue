16494359e162992492af7f8be8d561670b2e7875
T480Mac <942385056@qq.com>
2022-03-28 20:29:24 +0800

    使用Tabbar标签栏

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
-<div>123</div>
+<van-tabbar v-model="active">
+  <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+  <van-tabbar-item icon="search">标签</van-tabbar-item>
+  <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+  <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+</van-tabbar>
 </template>
+
 
 <style lang="scss">
 
 </style>
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const active = ref(1);
+    return { active };
+  },
+};
+</script>
