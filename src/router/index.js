0238c754ad6a29a5632702ae3ccea7a555d40b27
T480Mac <942385056@qq.com>
2022-03-30 17:37:20 +0800

    新增页面路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,73 @@ const routes = [
     path: "/",
   },
   {
+    path: "/login",
+    name: "login",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/user/user-login.vue"),
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/user/user-register.vue"),
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/home/home-index.vue"),
+  },
+  {
+    path: "/shop-cart",
+    name: "shopCart",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/shop/shop-cart.vue"),
+  },
+  {
+    path: "/address",
+    name: "address",
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/address/address-list.vue"
+      ),
+  },
+  {
+    path: "/address-edit",
+    name: "address-edit",
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/address/address-edit.vue"
+      ),
+  },
+  {
+    path: "/address-add",
+    name: "address-add",
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/address/address-add.vue"
+      ),
+  },
+  {
+    path: "/shop-info",
+    name: "shop-info",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/shop/shop-info.vue"),
+  },
+  {
+    path: "/order-list",
+    name: "order-list",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/order/order-list.vue"),
+  },
+
+  {
     // path: '/about',
     // name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../viewss/Aboutviews.vue')
   },
 ];
 
