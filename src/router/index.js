5008d754ec22731bddddb4f369a849f14d89fce0
T480Mac <942385056@qq.com>
2022-03-31 20:11:34 +0800

    设置登录校验跳转逻辑

--- src/router/index.js
+++ src/router/index.js
@@ -1,83 +1,92 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import {createRouter, createWebHashHistory} from "vue-router";
 
 const routes = [
-  {
-    path: "/",
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/login/Login.vue"),
-  },
-  {
-    path: "/register",
-    name: "Register",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/login/Register.vue"),
-  },
-  {
-    path: "/home",
-    name: "Home",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/home/Home.vue"),
-  },
-  {
-    path: "/shopCart",
-    name: "ShopCart",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/shop/ShopCart.vue"),
-  },
-  {
-    path: "/addressList",
-    name: "AddressList",
-    component: () =>
-      import(
-        /* webpackChunkName: "about" */ "../views/address/AddressList.vue"
-      ),
-  },
-  {
-    path: "/addressEdit",
-    name: "AddressEdit",
-    component: () =>
-      import(
-        /* webpackChunkName: "about" */ "../views/address/AddressEdit.vue"
-      ),
-  },
-  {
-    path: "/addressAdd",
-    name: "AddressAdd",
-    component: () =>
-      import(
-        /* webpackChunkName: "about" */ "../views/address/AddressAdd.vue"
-      ),
-  },
-  {
-    path: "/shop",
-    name: "Shop",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/shop/Shop.vue"),
-  },
-  {
-    path: "/order",
-    name: "Order",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/order/Order.vue"),
-  },
+    {
+        path: "/",
+        name: "Home",
+        component: () =>
+            import(/* webpackChunkName: "home" */ "../views/home/Home"),
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: () =>
+            import(/* webpackChunkName: "login" */ "../views/login/Login"),
+        beforeEnter:(to,from,next) =>{
+            const {isLogin}=localStorage
+            if (isLogin){
+                next({name:'Home'})
+            }else {
+                next()
+            }
+        }
+    },
+    {
+        path: "/register",
+        name: "Register",
+        component: () =>
+            import(/* webpackChunkName: "register" */ "../views/login/Register.vue"),
+    },
 
-  {
-    // path: '/about',
-    // name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../viewss/Aboutviews.vue')
-  },
+    {
+        path: "/shopCart",
+        name: "ShopCart",
+        component: () =>
+            import(/* webpackChunkName: "shopCart" */ "../views/shop/ShopCart.vue"),
+    },
+    {
+        path: "/addressList",
+        name: "AddressList",
+        component: () =>
+            import(
+                /* webpackChunkName: "addressList" */ "../views/address/AddressList.vue"
+                ),
+    },
+    {
+        path: "/addressEdit",
+        name: "AddressEdit",
+        component: () =>
+            import(
+                /* webpackChunkName: "addressEdit" */ "../views/address/AddressEdit.vue"
+                ),
+    },
+    {
+        path: "/addressAdd",
+        name: "AddressAdd",
+        component: () =>
+            import(
+                /* webpackChunkName: "addressAdd" */ "../views/address/AddressAdd.vue"
+                ),
+    },
+    {
+        path: "/shop",
+        name: "Shop",
+        component: () =>
+            import(/* webpackChunkName: "shop" */ "../views/shop/Shop.vue"),
+    },
+    {
+        path: "/order",
+        name: "Order",
+        component: () =>
+            import(/* webpackChunkName: "order" */ "../views/order/Order.vue"),
+    },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+    history: createWebHashHistory(),
+    routes,
 });
+router.beforeEach((to, from, next) => {
+    const {isLogin} = localStorage;
+    const {name} =to;
+    // console.log(isLogin)
+    // // 如果登录了
+    // if (isLogin||to.name==='Login'){
+    //     next()//访问原有路径地址
+    // }else{//没有登录的话跳转到登录页面
+    //     next({name:'Login'})
+    // }
+    isLogin||name==='Login'||name==='Register' ? next():next({name:'Login'})
+})
 
 export default router;
