9b476ae64fff6a89a873c36698437ee23d48f202
MacServer <942385056@qq.com>
2022-04-01 10:30:38 +0800

    封装axios新增post请求

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 const instance = axios.create({
   baseURL: "http://localhost:3000",
   timeout: 10000,
+  withCredentials:true//允许请求带cookie
 });
 
 const get = (url, params = {}) => {
@@ -17,4 +18,16 @@ const get = (url, params = {}) => {
       });
   });
 };
-export { get };
+const post = (url, params = {}) => {
+  return new Promise((resolve, reject) => {
+    instance
+      .post(url, params)
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+export { get ,post};
